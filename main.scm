8255e7a0601c627563b0e2120cd3ff5c2f46170d
Ali Ahmed <ali.nisar87@gmail.com>
2020-08-21 04:27:11 +0000

    fixed points and average damping

--- main.scm
+++ main.scm
@@ -655,7 +655,7 @@
 ;; b = 1 - y
 ;; f(x,y) = xa^2 + yb + ab
 
-(define (f x y)
+(define (f1 x y)
 	(define (f-helper a b)
 		(+ (* x (square a))
 		   (* y b)
@@ -688,4 +688,66 @@
 			 (* a b)
 		)
 	)
+)
+
+;; Exercise 1.34 
+
+(define (f g) (g 2))
+
+;; Finding roots by half-interval method
+
+(define (search f neg-point pos-point)
+	(let ((midpoint (average neg-point pos-point)))
+		(if (close-enough? neg-point pos-point)
+			midpoint
+			(let ((test-value (f midpoint)))
+				(cond ((positive? test-value) (search f neg-point))
+							((negative? test-value) (search f pos-point))
+							(else midpoint)
+				)
+			)
+		)
+	)
+)
+
+(define (close-enough? x y) (< (abs (- x y)) 0.001))
+
+(define (negative? x) (< x 0))
+(define (positive? x) (> x 0))
+
+(define (half-interval f a b)
+	(let ((a-value (f a))
+				(b-value (f b)))
+		(cond ((and (negative? a-value) (positive? b-value)) (search f a b))
+					((and (positive? a-value) (negative? b-value)) (search f b a))
+					(else (error "Values are not of opposite sign" a b))
+
+		)
+	)
+)
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+	(define (close-enough v1 v2)
+		(< (abs (- v1 v2)) tolerance)
+	)
+	(define (try guess)
+		(let ((next (f guess)))
+			(if (close-enough guess next)
+				next
+				(try next)
+			)
+		)
+	)
+	(try first-guess)
+)
+
+(define (sqrt-2 x)
+	(fixed-point (lambda (y) (average y (/ x y))) 1.0)
+)
+
+;; Exercise 1.35
+
+(define golden-ratio 
+	(fixed-point (lambda (y) (+ 1 (/ 1 y))) 1.0)
 )
