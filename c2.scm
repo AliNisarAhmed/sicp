157845d30ab9c0af15fcf3c8de6def4073f7113c
Ali Ahmed <ali.ahmed@punchcard.io>
2020-08-24 23:23:47 -0600

    exercise 2.7,8,2.9

--- c2.scm
+++ c2.scm
@@ -59,6 +59,73 @@
 
 
 
+;; Exercise 2.7
+
+(define (make-interval a b) (cons a b))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval
+   x
+   (make-interval (/ 1.0 (upper-bound y))
+                  (/ 1.0 (lower-bound y)))))
+
+(define (upper-bound i) (cdr i))
+
+(define (lower-bound i) (car i))
+
+
+;; Exercise 2.8
+
+
+(define (sub-interval x y)
+  (add-interval x (negate-interval y)))
+
+
+;; To negate an interval, we multiply both bounds by -1;
+;; but by doing so, the upper bound becomes the lower bound and vice versa
+;; so, we need to switch upper and lower bounds
+(define (negate-interval y)
+  (make-interval (* -1 (upper-bound y)) (* -1 (lower-bound y))))
+
+(define i1 (make-interval 4 10))
+(define i2 (make-interval 2 12))
+(define diff (sub-interval i1 i2))
+
+
+;; Exercise 2.9
+
+(define (width-interval i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
