8dacd9722868720d69ab6ce60d6b90fc062d95b5
Ali Ahmed <ali.ahmed@punchcard.io>
2020-09-04 23:50:34 -0600

    solved set as ordered list adjoin

--- c2.scm
+++ c2.scm
@@ -1302,10 +1302,107 @@
         (else (error "Infix expression required"))))
 
 
-(define (memq item x)
-  (cond ((null? x) false)
-        ((eq? item (car x)) x)
-        (else (memq item (cdr x)))))
+
+;; Exercise 2.59
+
+
+
+
+
+
+
+
+
+;; ---- SETS ----
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))
+        ))
+
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (adjoin-set (car set1) (union-set (cdr set1) set2)))
+        ))
+
+
+;; Exercise 2.60
+
+;; Representation that allows duplicates
+
+(define (element-d? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-d? x (cdr set)))))
+
+(define (adjoin-set-d x set)
+  (cons x set))
+
+(define (intersection-d set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-d? (car set1) set2)
+         (cons (car set1) (intersection-d (cdr set1) (filter (lambda (x) (not (= x (car set1)))) set2))))
+        (else (intersection-d (cdr set1) set2))
+        ))
+
+(define (union-d set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (append set1 set2))))
+
+
+;; ---- Representing set as ordered list (of numbers only) with unique elements
+
+(define (element-o? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-o? x (cdr set)))))
+
+
+(define (intersection-set-o set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set-o (cdr set1)
+                                          (cdr set2))))
+              ((< x1 x2)
+               (intersection-set-o (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set-o set1 (cdr set2)))))))
+
+
+;; Exercise 2.61
+
+(define (adj-set-o x set)
+  (if (null? set)
+     (list x)
+      (let ((y (car set)))
+        (cond ((= x y) set)
+              ((< x y) (cons x set))
+              (else (cons y (adj-set-o x (cdr set))))
+              ))))
+
+
+
+
+
+
 
 
 
