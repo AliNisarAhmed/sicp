f47e7ced706e16c6b37692ad41e04c8971c8c826
Ali Ahmed <ali.ahmed@punchcard.io>
2020-09-05 22:30:54 -0600

    Huffman encoding

--- c2.scm
+++ c2.scm
@@ -1400,10 +1400,244 @@
 
 
 
+;; Exercise 2.62
+
+(define (union-set-o set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+         (let ( (x (car set1)) (y (car set2)) )
+           (cond ((= x y) (cons x (union-set-o (cdr set1) (cdr set2))))
+                 ((< x y) (cons x (union-set-o (cdr set1) set2)))
+                 (else (cons y (union-set-o set1 (cdr set2))))
+                 )))))
+
+
+
+;; ---- Representing sets as Binary Trees ----
+
+
+(define (entry tree) (car tree))
+
+(define (left-branch-t tree) (cadr tree))
+(define (right-branch-t tree) (caddr tree))
+
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+
+(define (element-of-set-t? x set)
+  (cond ((null? set) false)
+        ((= x (entry set) true))
+        ((< x (entry set)) (element-of-set? x (left-branch-t set)))
+        (else (element-of-set? x (right-branch-t set)))
+        ))
+
+(define (adj-set-t x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set)) (make-tree (entry set)
+                                      (adj-set-t x (left-branch-t set))
+                                      (right-branch-t set)))
+        (else (make-tree (entry set)
+                         (left-branch-t set)
+                         (adj-set-t x (right-branch-t set))))
+        ))
+
+(define set-tree (make-tree 4 (make-tree 5 nil nil) (make-tree 6 nil nil) ))
+(define bt1 (make-tree 7
+                      (make-tree 3 (make-tree 1 nil nil) (make-tree 5 nil nil))
+                      (make-tree 9 nil (make-tree 11 nil nil))))
+
+
+(define bt2 (make-tree 3
+                       (make-tree 1 nil nil)
+                       (make-tree 7
+                                  (make-tree 5 nil nil)
+                                  (make-tree 9 nil
+                                             (make-tree 11 nil nil)))))
+
+
+(define bt3 (make-tree
+             5
+             (make-tree 3 (make-tree 1 nil nil) nil)
+             (make-tree 9
+                        (make-tree 7 nil nil)
+                        (make-tree 11 nil nil))))
+
+;; Exercise 2.63
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch-t tree))
+              (cons (entry tree) (tree->list-1 (right-branch-t tree)))))
+      )
+
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result)
+    (if (null? tree)
+        result
+        (copy-to-list (left-branch-t tree) (cons (entry tree)
+                                                 (copy-to-list (right-branch-t tree) result)))))
+  (copy-to-list tree '()))
 
 
 
 
+;; Exercise 2.64
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+
+(define odd-list (list 1 3 5 7 9 11))
+(define even-list (list 2 4 6 8 10 12))
+
+
+;; Exercise 2.65
+
+
+;; set1 and set2 are in tree form
+
+(define (union-set-t set1 set2)
+  (let ((l1 (tree->list-2 set1))
+        (l2 (tree->list-2 set2)))
+    (list->tree (union-set-o l1 l2))))
+
+(define (intersection-set-t set1 set2)
+  (let ((l1 (tree->list-2 set1))
+        (l2 (tree->list-2 set2)))
+    (list->tree (intersection-set-o l1 l2))))
+
+
+
+;; Sets and Information Retrieval
+
+;; set is unordered list
+
+;; Commented out because `key` is not defined
+#|
+
+
+(define (lookup k set)
+  (cond ((null? set) false)
+        ((equal? k (key (car set))) (car set))
+        (else (lookup k (cdr set)))
+        ))
+
+
+;; set is binary tree
+
+(define (lookup-t k set)
+  (cond ((null? set) false)
+        ((equal? k (key (entry set))) (entry set))
+        ((< k (key (entry set))) (lookup k (left-branch set)))
+        (else (lookup k (right-branch set)))
+        ))
+
+
+|#
+
+
+;; ------------ Huffman Encoding -----------------
+
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? node) (eq? 'leaf (car node)))
+(define (symbol-leaf node) (cadr node))
+(define (weight-leaf node) (caddr node))
+
+
+;; left is left branch of the tree, right is right branch
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))
+        ))
+
+(define (left-branch-c tree) (car tree))
+(define (right-branch-c tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+
+;; Decoding
+
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) next-branch))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit current-branch)
+  (cond ((= 1 bit) (right-branch-c current-branch))
+        ((= 0 bit) (left-branch-c current-branch))
+        (else (error "Bad bit"))))
+
+
+;; representing the list of symbols as an ordered (by weight) list
+(define (adjoin-set-c x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set) (adjoin-set-c x (cdr set))))
+        ))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)   ;; symbol
+                               (cadr pair)) ;; frequency
+                    (make-leaf-set (cdr pairs))))))
+
+
+;; Exercise 2.67
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree (make-leaf 'B 2)
+                                  (make-code-tree (make-leaf 'D 1)
+                                                  (make-leaf 'C 1)))))
+
+
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
 
 
